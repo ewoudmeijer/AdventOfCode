9c35c67bc800d014c443ebd407316937acce819a
Ewoud <ewoud@ewoudmeijer.nl>
2018-12-03 23:23:05 +0100

    first

---
+++ day02/first.ps1
@@ -0,0 +1,12 @@
+﻿[int]$total2 = 0;
+[int]$total3 = 0;
+Get-Content .\input.txt | ForEach-Object {
+    if (($_.GetEnumerator() | group -NoElement | Where-Object -Property Count -EQ 2).Count -gt 1) {
+      $total2++;
+    }
+    if (($_.GetEnumerator() | group -NoElement | Where-Object -Property Count -EQ 3).Count -gt 1) {
+      $total3++;
+    }
+ }
+echo ($total2*$total3);
+
